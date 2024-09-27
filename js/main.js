d69144d1ec3a2bb9e05b650488006fea4012e1df
KylieTKay <113371037+KylieTKay@users.noreply.github.com>
2024-09-27 08:42:35 -0400

    mobileHtml

--- js/main.js
+++ js/main.js
@@ -18,3 +18,52 @@
 	button.addEventListener("click", hamburgerMenu, false);
 		
 })();
+
+(function () {
+  function doTheShuffle() {
+    console.log('doTheShuffle')
+    // bindings off
+    $('.card-selected').off('click', doTheShuffle)
+    // bindings on
+    $('.card-selected').on('animationend webkitAnimationEnd', halfDoneTheShuffle)
+    // actual logic
+    $('#finalist').addClass('doTheShuffle')
+  }
+
+  function halfDoneTheShuffle() {
+    console.log('halfDoneTheShuffle')
+
+    // bindings off
+    $('.card-selected').off('animationend webkitAnimationEnd', halfDoneTheShuffle)
+
+    // mark it as unselected to set the correct z-index
+    $('.card-selected').removeClass('card-selected').addClass('card-unselected')
+
+    // wait until the next animation ends
+    $('.card-unselected').on('animationend webkitAnimationEnd', doneTheShuffle)
+  }
+
+  function doneTheShuffle() {
+    console.log('doneTheShuffle')
+
+    var unselectedCard, newSelectedCard
+
+    // bindings off
+    $('.card-unselected').off('animationend webkitAnimationEnd', doneTheShuffle)
+
+    // shuffle the cards in the DOM
+    $('#finalist').removeClass('doTheShuffle')
+    unselectedCard = $('.card-unselected').remove().removeClass('card-unselected')
+    $('.cardShuffle').prepend(unselectedCard)
+
+    // mark the top card as selected
+    newSelectedCard = $('.cardShuffle .card').last()
+    newSelectedCard.addClass('card-selected')
+
+    // bindings on
+    $('.card-selected').on('click', doTheShuffle)
+  }
+
+  // Initial binding
+  $('.card-selected').on('click', doTheShuffle)
+})();
